6c8a27dc02d5c2364de4e5298e144f86bcfd5bf2
Kaelesty <74826130+Kaelesty@users.noreply.github.com>
2023-07-19 21:17:19 +0300

    Initial commit

---
+++ app/src/main/java/com/kaelesty/cryptocurrencies_observer/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kaelesty.cryptocurrencies_observer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //  Crypto Compare API key
+    // d59907d2061109dbc04cce033b0cfb94dbd9ed01c099d20269ad81eb672f4054
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
